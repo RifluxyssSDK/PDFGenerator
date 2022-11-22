c41aeeeafd4012c82c7498dec4828ec955a625f0
kishanth <s.kishanth@rifluxyss.com>
2022-11-22 15:08:52 +0530

    PDF Generator Initialize Commits

--- app/src/main/java/com/pdfutil/MainActivity.java
+++ app/src/main/java/com/pdfutil/MainActivity.java
@@ -5,11 +5,18 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.rifluxyss.pdfgenerator.constants.PageSize;
+import com.rifluxyss.pdfgenerator.mainView.PdfWriter;
+import com.rifluxyss.pdfgenerator.structure.Table;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PdfWriter pdfWriter = new PdfWriter(PageSize.A4);
+        Table table = pdfWriter.createTable(20);
     }
 }
