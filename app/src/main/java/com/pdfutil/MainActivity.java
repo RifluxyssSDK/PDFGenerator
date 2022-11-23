49abd6107b8f3ca70baa9d11ec27d340dacc2e64
Mukesh <p.mukeshkannan@rifluxyss.com>
2022-11-23 16:36:39 +0530

    New Main Branch

--- app/src/main/java/com/pdfutil/MainActivity.java
+++ app/src/main/java/com/pdfutil/MainActivity.java
@@ -1,8 +1,18 @@
 package com.pdfutil;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.android.core.Document;
+import com.android.io.AreaBreak;
+import com.android.io.Paragraph;
+import com.android.io.Text;
+import com.android.kernel.FontStyle;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +22,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Document document = new Document().init(this);
+
+        document.open(10);
+
+        document.setPadding(20);
+
+        document.add(new Paragraph()
+                .add(new Text(1,10,"Hello World").setFontStyle(FontStyle.HELVETICA))
+        );
+
+        document.add(new AreaBreak());
+
+        document.add(new Paragraph()
+                .add(new Text(1,10,"Hello World").setFontStyle(FontStyle.HELVETICA))
+        );
+
+        document.close();
+
+        try {
+            document.finish(new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),"Doc.pdf"));
+            Toast.makeText(this, "Doc Created", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
